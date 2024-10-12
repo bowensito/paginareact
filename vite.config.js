67486b718f990d88ad95b0af6ae01ad39406dcd7
bowensito <bowensitosmurf@gmail.com>
2024-10-12 17:04:12 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://bowensito.github.io/paginareact',
+  base: 'paginareact',
   plugins: [react()],
 });
