ff19b90603181766d874a98082afab042554e782
bowensito <bowensitosmurf@gmail.com>
2024-10-16 10:47:46 -0300

    Add files via upload

--- src/components/FormularioPelicula.jsx
+++ src/components/FormularioPelicula.jsx
@@ -1,41 +1,60 @@
 import React from 'react';
 
 const FormularioPelicula = ({ peliculaEditar, nuevaPelicula, cambio, agregarPelicula, actualizarPelicula }) => {
+
+  const manejarSubmit = (e) => {
+    e.preventDefault(); //recargar pagina
+    if (peliculaEditar) {
+      actualizarPelicula();
+    } else {
+      agregarPelicula();
+    }
+  };
+
   return (
-    <div>
+    <form onSubmit={manejarSubmit}>
       <h2>{peliculaEditar ? 'Editar Película' : 'Agregar Película'}</h2>
+      
       <input
         type="text"
         name="titulo"
         placeholder="Título"
         value={nuevaPelicula.titulo}
         onChange={cambio}
+        required
       />
+      
       <input
         type="text"
         name="fechaEstreno"
         placeholder="Fecha de Estreno"
         value={nuevaPelicula.fechaEstreno}
         onChange={cambio}
+        required
       />
+      
       <input
         type="text"
         name="productor"
         placeholder="Productor"
         value={nuevaPelicula.productor}
         onChange={cambio}
+        required
       />
+      
       <input
-        type="text"
+        type="url"
         name="portada"
         placeholder="URL de la portada"
         value={nuevaPelicula.portada}
         onChange={cambio}
+        required
       />
-      <button onClick={peliculaEditar ? actualizarPelicula : agregarPelicula}>
+      
+      <button type="submit">
         {peliculaEditar ? 'Actualizar Película' : 'Agregar Película'}
       </button>
-    </div>
+    </form>
   );
 };
 
